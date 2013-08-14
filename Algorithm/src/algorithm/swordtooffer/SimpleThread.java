cdef50f95cac8c9b479326193bcef53c07262609
Li Yan <lostking1990@gmail.com>
2013-08-14 18:03:53 +0800

    Create SimpleThread.java

---
+++ Algorithm/src/algorithm/swordtooffer/SimpleThread.java
@@ -0,0 +1,29 @@
+
+public class SimpleThread extends Thread {
+	private int value;
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public SimpleThread(int value) {
+		this.value = value;
+	}
+	
+	public void run() {
+		while(true) {
+			synchronized(this) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(value + " ");
+			}
+		}
+	}
+}
