a1b51fdbda0de59e50b35cf90ba84d1e5ef8e57c
Li Yan <lostking1990@gmail.com>
2013-08-14 18:04:47 +0800

    Create Scheduler.java

---
+++ Algorithm/src/algorithm/swordtooffer/Scheduler.java
@@ -0,0 +1,26 @@
+
+public class Scheduler {
+
+	public static void main(String[] args) {
+		SimpleThread[] threads = new SimpleThread[3];
+		for(int i=0; i<3; i++) {
+			threads[i] = new SimpleThread(i+1);
+			threads[i].start();
+		}
+		
+		int index = 0;
+		while(true) {
+			synchronized(threads[index]) {
+				threads[index].notify();
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			index = (index+1)%3;
+		}	
+	}
+
+}
